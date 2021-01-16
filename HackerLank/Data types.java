be188d6544a346d5fcf277759635a59359973562
chanqun <cksgns93@naver.com>
2021-01-16 23:53:58 +0900

    data types solve

---
+++ HackerLank/Data types.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        int i2;
+        double d2;
+        String s2;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        scan.nextLine();
+        s2 = scan.nextLine();
+        
+        System.out.println(i+i2);
+        System.out.println(d+d2);
+        System.out.println(s+s2);
+        
+        scan.close();
+    }
+}
